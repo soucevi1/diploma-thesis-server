4973219ecdc406a6d36d683a479dd85e699cee86
Vít Souček <soucevi1@fit.cvut.cz>
2019-10-20 12:21:44 +0200

    Funguje i po vypnuti, je tam ale ozvena

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ package com.company;
 import java.io.ByteArrayInputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.util.Arrays;
 
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
@@ -19,7 +20,8 @@ class Server {
     static AudioFormat format;
     static boolean status = true;
     static int port = 50005;
-    static int sampleRate = 8000;//44100;//44100;
+    static int sampleRate = 44100;//16000;//8000;//44100;//44100;
+    static int bufferLen = 26880*20;//960*20;//512*2;
 
     static DataLine.Info dataLineInfo;
     static SourceDataLine sourceDataLine;
@@ -40,10 +42,10 @@ class Server {
          * 8000+512
          */
 
-
-        byte[] receiveData = new byte[512];
+        byte[] receiveData = new byte[bufferLen];
 
         format = new AudioFormat(sampleRate, 16, 1, true, false);
+
         dataLineInfo = new DataLine.Info(SourceDataLine.class, format);
         sourceDataLine = (SourceDataLine) AudioSystem.getLine(dataLineInfo);
         sourceDataLine.open(format);
@@ -52,22 +54,33 @@ class Server {
         FloatControl volumeControl = (FloatControl) sourceDataLine.getControl(FloatControl.Type.MASTER_GAIN);
         volumeControl.setValue(volumeControl.getMaximum());
 
-        DatagramPacket receivePacket = new DatagramPacket(receiveData,
-                receiveData.length);
         while (status == true) {
-            ByteArrayInputStream baiss = new ByteArrayInputStream(
-                    receivePacket.getData());
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
             serverSocket.receive(receivePacket);
-            ais = new AudioInputStream(baiss, format, receivePacket.getLength());
-            toSpeaker(receivePacket.getData());
+            System.out.println("Received bytes: " + receivePacket.getLength());
+
+            /*ByteArrayInputStream baiss = new ByteArrayInputStream(
+                    receivePacket.getData());
+            ais = new AudioInputStream(baiss, format, receivePacket.getLength());*/
+            //printData(receivePacket.getData());
+            toSpeaker(receivePacket.getData(), receivePacket.getLength());
+            Arrays.fill(receiveData, (byte)0);
         }
         sourceDataLine.drain();
         sourceDataLine.close();
     }
 
-    public static void toSpeaker(byte soundbytes[]) {
+    public static void printData(byte data[], int length){
+        System.out.print("Received: ");
+        for (byte datum : data) {
+            System.out.print(String.format("%02X", datum));
+        }
+        System.out.println("");
+    }
+
+    public static void toSpeaker(byte soundbytes[], int length) {
         try {
-            sourceDataLine.write(soundbytes, 0, soundbytes.length);
+            sourceDataLine.write(soundbytes, 0, length);
         } catch (Exception e) {
             System.out.println("Not working in speakers...");
             e.printStackTrace();
